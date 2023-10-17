185bc0e9166f41e066666fbc9886a41a593c7cad
vincm1 <vincent.michler94@gmail.com>
2023-10-17 20:16:36 +0200

    adapted user dashboard UX

--- iqsheets_app/dashboard/forms.py
+++ iqsheets_app/dashboard/forms.py
@@ -1,13 +1,14 @@
+""" Forms for DB """
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, TextAreaField, StringField, SelectField, SubmitField, ValidationError
+from wtforms import HiddenField, TextAreaField, SelectField, RadioField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class DashboardForm(FlaskForm):
-    excel_google = info_prompt = SelectField('Was möchtest du generieren?', validators=[DataRequired()], choices=["Excel", "Google Sheets"], render_kw={'class': 'btn-group-toggle', 'data-toggle': 'buttons'})
-    info_prompt = SelectField('Was möchtest du generieren?', validators=[DataRequired()], choices=["Formel", "VBA"], render_kw={'class': 'btn-group-toggle', 'data-toggle': 'buttons'})
-    formula_explain = SelectField('Formel oder Erklärung', validators=[DataRequired()], choices=["Erstellen", "Erklären"], render_kw={'class': 'btn-group-toggle', 'data-toggle': 'buttons'})
-    prompt = TextAreaField('Funktion die Zelle A1 mit Zelle B1 summiert z.B.', validators=[DataRequired(), Length(min=10, max=400)], render_kw={"style": "min-height: 10rem;"})
-    submit = SubmitField('Formel')  
+    excel_google = RadioField('Die Formel/Erklärung ist für...?', validators=[DataRequired()], choices=["Excel", "Google Sheets"], render_kw={'class': 'form-check-input'})
+    info_prompt = RadioField('Was möchtest du generieren?', validators=[DataRequired()], choices=["Formel", "VBA"], render_kw={'class': 'form-check-input'})
+    formula_explain = RadioField('Formel oder Erklärung', validators=[DataRequired()], choices=["Erstellen", "Erklären"], render_kw={'class': 'form-check-input'})
+    prompt = TextAreaField('Welches Problem versuchen Sie zu lösen oder welche Formel wollen Sie verstehen? Versuchen Sie, so genau wie möglich zu sein.', validators=[DataRequired(), Length(min=10, max=400)], render_kw={"style": "min-height: 10rem;"})
+    submit = SubmitField('Formel generieren')  
 class FeedbackForm(FlaskForm):
     provider = HiddenField()
     favorite_type = HiddenField()
